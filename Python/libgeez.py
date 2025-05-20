5b3845be1bc686759155253079cdca3bc7f7176a
MoolmanM <mmoolman067@gmail.com>
2025-05-20 22:22:17 +0200

    feat: add GitBlob

--- Python/libgeez.py
+++ Python/libgeez.py
@@ -228,3 +228,12 @@ def object_write(obj, repo=None):
                 # Compress and write
                 f.write(zlib.compress(result))
     return sha
+
+def GitBlob(GitObject):
+    fmt=b'blob'
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data

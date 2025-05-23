1c496ce8111c1d87064a695c85e071d9f3237f40
MoolmanM <mmoolman067@gmail.com>
2025-05-23 14:55:04 +0200

    feat: add cmd_hash_object

--- Python/libgeez.py
+++ Python/libgeez.py
@@ -306,4 +306,26 @@ argsp.add_argument("-w",
 argsp.add_argument("path",
                    help="Read object from <file>")
 
+def cmd_hash_object(args):
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
 
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+def object_hash(fd, fmt, repo=None):
+    """Hash object, writing it to repo if provided."""
+    data = fd.read()
+
+    # Choose contructor according the fmt argument
+    match fmt:
+        case b'commit'  : obj=GitCommit(data)
+        case b'tree'    : obj=GitTree(data)
+        case b'tag'     : obj=GitTag(data)
+        case b'blob'    : obj=GitBlob(data)
+        case _: raise Exception(f"Unknown type {fmt}!")
+            
+    return object_write(obj, repo)
